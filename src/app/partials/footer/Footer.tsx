4790fb3f5274a1291e37ec4cda330bc338589c52
Sabarna Chakravarty <sabarnac@gmail.com>
2019-09-29 20:23:57 +0530

    Set Footer Links To Open In New Tab

--- src/app/partials/footer/Footer.tsx
+++ src/app/partials/footer/Footer.tsx
@@ -4,12 +4,18 @@ import "./Footer.css"
 export default (): JSX.Element => (
   <div className="site footer">
     <div className="item">
-      <a href="https://github.com/sabarnac/spotify-batch-unfollow-app">
+      <a
+        href="https://github.com/sabarnac/spotify-batch-unfollow-app"
+        target="_blank"
+      >
         GitHub
       </a>
     </div>
     <div className="item">
-      <a href="https://github.com/sabarnac/spotify-batch-unfollow-app/issues">
+      <a
+        href="https://github.com/sabarnac/spotify-batch-unfollow-app/issues"
+        target="_blank"
+      >
         Report Issue
       </a>
     </div>
