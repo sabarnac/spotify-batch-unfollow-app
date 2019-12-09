972df544e8afe700de340653eaa99f3dc286674a
Sabarna Chakravarty <sabarnac@gmail.com>
2019-12-10 01:54:21 +0530

    Update Dependencies

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="react-scripts" />
-
-import { Dispatch } from "react"
-
-export type Nullable<T> = T | null | undefined
-export type ReactState<T> = [T, Dispatch<T>]
-export type RequestQueryParams = {
-  [name: string]: string | number | null | undefined
-}
