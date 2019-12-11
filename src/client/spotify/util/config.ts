e5eabd0125ca3e3acaf7afdbb9f526c84d1af6f5
Sabarna Chakravarty <sabarnac@gmail.com>
2019-12-11 13:55:19 +0530

    Update Client Call Batch Size

--- src/client/spotify/util/config.ts
+++ src/client/spotify/util/config.ts
@@ -1,5 +1,5 @@
 export const SPOTIFY_API_URL: string = "https://api.spotify.com/v1";
-export const BATCH_SIZE: number = 1;
+export const BATCH_SIZE: number = 50;
 
 export default new (class {
   public userToken: string | undefined = undefined;
