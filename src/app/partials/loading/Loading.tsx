1b4a70ceb1033f78973da051941f3b4e41ff6e6a
Sabarna Chakravarty <sabarnac@gmail.com>
2019-12-11 14:06:26 +0530

    Update Loading Icon
    * Change loading icon color to white.
    * Add a padding to the loading icon when used inside AllArtistFollows.

--- src/app/partials/loading/Loading.tsx
+++ src/app/partials/loading/Loading.tsx
@@ -1,14 +1,16 @@
-import React from "react";
 import "./Loading.css";
+
+import React from "react";
 import { ClipLoader } from "react-spinners";
 
 interface LoadingProps {
   text?: string;
+  color?: string;
 }
 
-export default ({ text }: LoadingProps) => (
+export default ({ text, color = "#d8f3e0" }: LoadingProps) => (
   <div className="loading">
-    <ClipLoader sizeUnit={"rem"} size={3} />
+    <ClipLoader sizeUnit={"rem"} size={3} color={color} />
     {text && <div>{text}</div>}
   </div>
 );
