ddfcf5ad5854994775f7be70913fea462b10319f
Sabarna Chakravarty <sabarnac@gmail.com>
2020-01-11 16:08:51 +0530

    Updates

---
+++ precache-manifest.8cb09ff856b3fa2fafd6640706e26a67.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fe4e8a83dfc570ab99903587d18af9",
+    "url": "https://spotify-bulk-unfollow.com/index.html"
+  },
+  {
+    "revision": "b972f7234057557be0d0",
+    "url": "https://spotify-bulk-unfollow.com/static/css/3.c7d8627d.chunk.css"
+  },
+  {
+    "revision": "9463b74162484a2e9f8a",
+    "url": "https://spotify-bulk-unfollow.com/static/css/6.91d1d98b.chunk.css"
+  },
+  {
+    "revision": "a0cfec5257a7db418a6d",
+    "url": "https://spotify-bulk-unfollow.com/static/css/7.fb4c0ff2.chunk.css"
+  },
+  {
+    "revision": "b2dc9135ce6ff70b87aa",
+    "url": "https://spotify-bulk-unfollow.com/static/css/8.ab60ec18.chunk.css"
+  },
+  {
+    "revision": "52adeb718f9166ca9b72",
+    "url": "https://spotify-bulk-unfollow.com/static/css/main.2a897f00.chunk.css"
+  },
+  {
+    "revision": "c6dc2d56399fa2342383",
+    "url": "https://spotify-bulk-unfollow.com/static/js/2.a78645ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https://spotify-bulk-unfollow.com/static/js/2.a78645ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b972f7234057557be0d0",
+    "url": "https://spotify-bulk-unfollow.com/static/js/3.5c5ef48a.chunk.js"
+  },
+  {
+    "revision": "5672bfba5b28d030e8c1",
+    "url": "https://spotify-bulk-unfollow.com/static/js/4.0a56c927.chunk.js"
+  },
+  {
+    "revision": "8c88da6d48b54c1432224ce9157629c8",
+    "url": "https://spotify-bulk-unfollow.com/static/js/4.0a56c927.chunk.js.LICENSE"
+  },
+  {
+    "revision": "016ab0da64d48705c9b0",
+    "url": "https://spotify-bulk-unfollow.com/static/js/5.ac1b1a17.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "https://spotify-bulk-unfollow.com/static/js/5.ac1b1a17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9463b74162484a2e9f8a",
+    "url": "https://spotify-bulk-unfollow.com/static/js/6.fcf4f4c5.chunk.js"
+  },
+  {
+    "revision": "a0cfec5257a7db418a6d",
+    "url": "https://spotify-bulk-unfollow.com/static/js/7.bac88af5.chunk.js"
+  },
+  {
+    "revision": "b2dc9135ce6ff70b87aa",
+    "url": "https://spotify-bulk-unfollow.com/static/js/8.f91cd748.chunk.js"
+  },
+  {
+    "revision": "52adeb718f9166ca9b72",
+    "url": "https://spotify-bulk-unfollow.com/static/js/main.16061ab1.chunk.js"
+  },
+  {
+    "revision": "42a5a009d9c34845b4c6",
+    "url": "https://spotify-bulk-unfollow.com/static/js/runtime-main.f04d25a4.js"
+  },
+  {
+    "revision": "c2e032c479db2bc429450abe9c077075",
+    "url": "https://spotify-bulk-unfollow.com/static/media/default.c2e032c4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.052d8f6fc91bb04ac0911dc2eafcd1e3.js"
+  "https://spotify-bulk-unfollow.com/precache-manifest.8cb09ff856b3fa2fafd6640706e26a67.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://spotify-bulk-unfollow.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
