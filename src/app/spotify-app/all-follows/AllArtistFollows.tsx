0784bdd60375e142e94caa758aa152cc51e3a81b
Sabarna Chakravarty <sabarnac@gmail.com>
2020-01-14 19:23:19 +0530

    Show Error Message When No Artists Are Being Followed

--- src/app/spotify-app/all-follows/AllArtistFollows.tsx
+++ src/app/spotify-app/all-follows/AllArtistFollows.tsx
@@ -1,16 +1,16 @@
-import "./AllArtistFollows.css";
+import "./AllArtistFollows.css"
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
 
-import { useGetAllUserArtistFollows } from "../../../client/spotify";
-import { Artist } from "../../../client/spotify/model";
-import Loading from "../../partials/loading/Loading";
-import ArtistList from "./partials/ArtistList";
+import { useGetAllUserArtistFollows } from "../../../client/spotify"
+import { Artist } from "../../../client/spotify/model"
+import Loading from "../../partials/loading/Loading"
+import ArtistList from "./partials/ArtistList"
 
 interface AllArtistFollowsProps {
-  selectedArtists: Artist[];
-  addArtistsForRemoval: (...artists: Artist[]) => void;
-  removeArtistsForRemoval: (...artists: Artist[]) => void;
+  selectedArtists: Artist[]
+  addArtistsForRemoval: (...artists: Artist[]) => void
+  removeArtistsForRemoval: (...artists: Artist[]) => void
 }
 
 export default ({
@@ -18,27 +18,27 @@ export default ({
   addArtistsForRemoval,
   removeArtistsForRemoval,
 }: AllArtistFollowsProps): JSX.Element => {
-  const [artistsResult, loading, error] = useGetAllUserArtistFollows();
-  const [artists, setArtists] = useState<Artist[]>([]);
+  const [artistsResult, loading, error] = useGetAllUserArtistFollows()
+  const [artists, setArtists] = useState<Artist[]>([])
 
   const addArtistToRemovalList = (artist: Artist) => {
-    addArtistsForRemoval(artist);
-  };
+    addArtistsForRemoval(artist)
+  }
   const removeArtistFromRemovalList = (artist: Artist) => {
-    removeArtistsForRemoval(artist);
-  };
+    removeArtistsForRemoval(artist)
+  }
 
   useEffect(() => {
     if (artistsResult.length === 0) {
-      return;
+      return
     }
 
-    const newArtists = artistsResult[artistsResult.length - 1];
+    const newArtists = artistsResult[artistsResult.length - 1]
 
-    setArtists(artists => [...artists, ...newArtists]);
+    setArtists(artists => [...artists, ...newArtists])
 
-    addArtistsForRemoval(...newArtists);
-  }, [addArtistsForRemoval, artistsResult]);
+    addArtistsForRemoval(...newArtists)
+  }, [addArtistsForRemoval, artistsResult])
 
   return (
     <div className="all-artist-follows">
@@ -53,6 +53,9 @@ export default ({
         </div>
       )}
       {!error && loading && artists.length === 0 && <Loading />}
+      {!error && !loading && artists.length === 0 && (
+        <div className="error loading-message">No followed artists found</div>
+      )}
       {!error && artists.length > 0 && (
         <ArtistList
           loadingResults={!error && loading}
@@ -63,5 +66,5 @@ export default ({
         />
       )}
     </div>
-  );
-};
+  )
+}
