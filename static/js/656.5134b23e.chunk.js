47f0ede075dc903412c24de75bb5dac7d69d134d
sabarnac <sabarnac@gmail.com>
2022-08-09 11:31:25 +0000

    Deploying to gh-pages from @ sabarnac/spotify-batch-unfollow-app@6dee06fc374890863ba3a88d3b0c833eb877c89d 🚀

---
+++ static/js/656.5134b23e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkspotify_batch_unfollow_app=self.webpackChunkspotify_batch_unfollow_app||[]).push([[656],{4656:function(n,o,e){e.r(o),e.d(o,{default:function(){return l}});e(2791);var t=e(4547),a=e(184),c=function(){t.ZP.resetToken(),localStorage.clear(),sessionStorage.clear()},l=function(){return(0,a.jsx)("div",{className:"logout",children:(0,a.jsx)("button",{className:"warning",onClick:c,children:"Logout"})})}}}]);
+//# sourceMappingURL=656.5134b23e.chunk.js.map
