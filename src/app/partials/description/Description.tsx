9ed6ca0f278f719248683c4e5e06f3b5fd5de8bc
Sabarna Chakravarty <sabarnac@gmail.com>
2023-04-14 18:49:57 +0100

    Update description

--- src/app/partials/description/Description.tsx
+++ src/app/partials/description/Description.tsx
@@ -3,7 +3,10 @@ import "./Description.css";
 import React from "react";
 
 const Description = (): JSX.Element => (
-  <div className="site description">Third-party app that lets you unfollow artists in bulk, instead of doing it one at a time.</div>
+  <div className="site description">
+    Third-party app that lets you unfollow artists in bulk, instead of doing it one at a time. You can choose not to
+    unfollow certain artists by deselecting them before unfollowing.
+  </div>
 );
 
 export default Description;
