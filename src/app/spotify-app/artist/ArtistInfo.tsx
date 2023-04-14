248b48450697f4a4e87713b223aceb254ba36555
Sabarna Chakravarty <sabarnac@gmail.com>
2023-04-14 18:37:18 +0100

    Fix ESLint Issue

--- src/app/spotify-app/artist/ArtistInfo.tsx
+++ src/app/spotify-app/artist/ArtistInfo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 import { Artist } from "../../../client/spotify/model";
 import DefaultImage from "./default.png";
-import SpotifyLogo from "./spotify-logo.png"
+import SpotifyLogo from "./spotify-logo.png";
 
 export enum ArtistStatus {
   SELECTED = "selected",
@@ -21,17 +21,25 @@ interface ArtistProps {
 }
 
 const ArtistInfo = ({ artist, status, onClick, description }: ArtistProps): JSX.Element => (
-  <div className={classNames("artist", status, { clickable: !!onClick })} onClick={(e) => {
-    if ((e.target as any).classList.contains("artist-link") || (e.target as any).classList.contains("spotify-link-logo")) {
-      return;
-    }
-    return onClick?.();
-  }}>
+  <div
+    className={classNames("artist", status, { clickable: !!onClick })}
+    onClick={(e) => {
+      if (
+        (e.target as any).classList.contains("artist-link") ||
+        (e.target as any).classList.contains("spotify-link-logo")
+      ) {
+        return;
+      }
+      return onClick?.();
+    }}
+  >
     <div className="avatar">
       <img src={artist.images[0]?.url ?? DefaultImage} alt={`Artist ${artist.name}`} />
     </div>
     <h3 className="name">{artist.name}</h3>
-    <a className="artist-link" href={artist.external_urls.spotify} target="_blank">View on <img className="spotify-link-logo" src={SpotifyLogo} alt="Spotify Logo"/></a>
+    <a className="artist-link" href={artist.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+      View on <img className="spotify-link-logo" src={SpotifyLogo} alt="Spotify Logo" />
+    </a>
     {description && <div className="description">{description}</div>}
   </div>
 );
