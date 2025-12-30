5c3b0432107ed0bf3c1c70cb0ebead1990aaf9f8
Sabarna Chakravarty <sabarnac@gmail.com>
2025-12-30 22:43:53 +0000

    Update Spotify Constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SPOTIFY_AUTH_DOMAIN: string = "accounts.spotify.com";
 export const SPOTIFY_CLIENT_ID: string = process.env.REACT_APP_CLIENT_ID || "";
-export const SPOTIFY_AUTHORIZE_URL: string = "https://accounts.spotify.com/authorize";
-export const SPOTIFY_TOKEN_URL: string = "https://accounts.spotify.com/api/token";
+export const SPOTIFY_AUTHORIZE_URL: string = `https://${SPOTIFY_AUTH_DOMAIN}/authorize`;
+export const SPOTIFY_TOKEN_URL: string = `https://${SPOTIFY_AUTH_DOMAIN}/api/token`;
 export const SPOTIFY_REDIRECT_URL: string = new URL(document.location.href).origin + "/callback";
